703eff4a5b893bde7f8ecdaa5a107bad1d42fa6d
Rahul <rahulkumar397222@gmail.com>
2024-09-09 18:36:37 +0530

    inital commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MasterComponent } from './master/master.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component:MasterComponent}
+];
